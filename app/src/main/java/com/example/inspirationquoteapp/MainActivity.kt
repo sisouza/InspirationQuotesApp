b3e178b778d4e9ce6773ed3ecea6d783d4dcaa4f
Sidia <sidia.reis1@gmail.com>
2022-02-10 21:05:42 -0800

    chore:create structure

---
+++ app/src/main/java/com/example/inspirationquoteapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.inspirationquoteapp
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.inspirationquoteapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
