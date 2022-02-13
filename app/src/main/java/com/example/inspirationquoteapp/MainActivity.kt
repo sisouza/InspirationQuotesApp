debe98782585e77e827f6a061884db45007c741c
Sidia <sidia.reis1@gmail.com>
2022-02-12 16:55:55 -0800

    feat:Check list size

--- app/src/main/java/com/example/inspirationquoteapp/MainActivity.kt
+++ app/src/main/java/com/example/inspirationquoteapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.inspirationquoteapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.inspirationquoteapp.data.Datasource
 import com.example.inspirationquoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val textView = binding.tvAffirmationsSize
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
