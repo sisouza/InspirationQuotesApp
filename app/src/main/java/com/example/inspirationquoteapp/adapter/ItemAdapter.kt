6d22f6da5090304756aa702ce4e237f6d394a35b
Sidia <sidia.reis1@gmail.com>
2022-02-12 18:49:59 -0800

    feat:Init Adapter and ViewHolder class

--- app/src/main/java/com/example/inspirationquoteapp/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/inspirationquoteapp/adapter/ItemAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.inspirationquoteapp.adapter
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.inspirationquoteapp.R
 import com.example.inspirationquoteapp.model.Affirmation
 
 
@@ -20,5 +24,16 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
      *  that can be passed into an ItemAdapter instance
      * **/
 
+    /**
+     * RecyclerView doesn't interact directly with item views, but deals with ViewHolders instead.
+     * A ViewHolder represents a single list item view in RecyclerView, and can be reused when possible
+     *
+     *  A ViewHolder instance holds references to the individual views within a list item layout
+     * **/
 
-}
+    //ItemViewHolder is a subclass of RecyclerView.ViewHolder the view parameter is passed into the superclass constructor.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.tvItemtitle)
+    }
+
+    }
