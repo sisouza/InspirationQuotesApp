9d8c06aa9beba69c2190622a3bce24de2c75cb31
Sidia <sidia.reis1@gmail.com>
2022-02-12 19:20:52 -0800

    feat:Init set MainActivity to use RecyclerView

--- app/src/main/java/com/example/inspirationquoteapp/MainActivity.kt
+++ app/src/main/java/com/example/inspirationquoteapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.inspirationquoteapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.RecyclerView
 import com.example.inspirationquoteapp.data.Datasource
 import com.example.inspirationquoteapp.databinding.ActivityMainBinding
 
@@ -16,6 +17,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // create and display items in the RecyclerView
+
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvWordsList)
+
+
+
 
     }
 }
