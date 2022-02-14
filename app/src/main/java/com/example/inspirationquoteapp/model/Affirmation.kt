7320832ed9ac1d904e56ae6620bee4671ac2b79b
Sidia <sidia.reis1@gmail.com>
2022-02-13 21:10:14 -0800

    feat:Add Resource Annotations

--- app/src/main/java/com/example/inspirationquoteapp/model/Affirmation.kt
+++ app/src/main/java/com/example/inspirationquoteapp/model/Affirmation.kt
@@ -1,7 +1,14 @@
 package com.example.inspirationquoteapp.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
-data class Affirmation(val stringResourceId: Int, val imageResourceId: Int) {
+
+/**
+ * Resource annotations add additional info to classes, methods, or parameters
+ *  and prevents the caller from accidentally pass  the arguments in the wrong order
+ * **/
+data class Affirmation(@StringRes val stringResourceId: Int,  @DrawableRes val imageResourceId: Int) {
 
 
 }
