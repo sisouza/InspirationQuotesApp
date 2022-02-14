4debf19a5b1b4a4b3ff1b4213c6ed8db3c9eca90
Sidia <sidia.reis1@gmail.com>
2022-02-13 21:13:12 -0800

    feat:Set image id parameter in datasource

--- app/src/main/java/com/example/inspirationquoteapp/data/Datasource.kt
+++ app/src/main/java/com/example/inspirationquoteapp/data/Datasource.kt
@@ -7,9 +7,9 @@ class Datasource {
 
     fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3)
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3)
         )
     }
 }
