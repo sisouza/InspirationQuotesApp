dc5f5057c105a50b6f0a20ad3128b6c8199e872a
Sidia <sidia.reis1@gmail.com>
2022-02-14 21:01:55 -0800

    test:Create List Quotes Test

--- app/src/androidTest/java/com/example/inspirationquoteapp/QuotesListTest.kt
+++ app/src/androidTest/java/com/example/inspirationquoteapp/QuotesListTest.kt
@@ -1,8 +1,15 @@
 package com.example.inspirationquoteapp
 
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -11,5 +18,16 @@ class QuotesListTest {
 
     @get:Rule
     val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun scroll_to_item() {
+        onView(withId(R.id.rvWordsList)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(4))
+
+        onView(withText(R.string.affirmation1))
+            .check(matches(isDisplayed()))
+
+    }
 
 }
